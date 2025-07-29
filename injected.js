a4e14fabcdc1221b84a0ae103951cd17403844ae
Derin Eryilmaz <mathia.is.fun@gmail.com>
2025-07-28 21:29:29 -0400

    ok well search is fine too

--- injected.js
+++ injected.js
@@ -66,7 +66,7 @@ enforceCache()
 chrome.runtime.sendMessage({ id: "get-status", hostname: location.hostname }, (response) => {
     // temporary fix for google.com
     if (location.hostname === "www.google.com"
-        && location.pathname === "/"
+        && ["/", "/search"].contains(location.pathname)
         && document.querySelector('meta[name=color-scheme]')?.content !== "dark light") {
         response.enabled = true
     }
